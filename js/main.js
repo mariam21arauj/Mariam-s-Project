788b93283abac90eaced825aa9ca719ded75177c
mariam21arauj <mariam21betancourt@gmail.com>
2022-10-21 22:21:04 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  function showPopup() {
+    $(".pop-up").addClass("show");
+    $(".pop-up-wrap").addClass("show");
+  }
+
+  $("#close").click(function () {
+    $(".pop-up").removeClass("show");
+    $(".pop-up-wrap").removeClass("show");
+  });
+
+  setTimeout(showPopup, 1000);
+});
